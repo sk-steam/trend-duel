abf0ff14169ad017e0c28971673b05ced5ff6a2b
Kyki2841 <nesarik50@gmail.com>
2026-09-25 20:28:40 +0300

    Use public STUN servers without registration

--- dist/rtc-config.js
+++ dist/rtc-config.js
@@ -1,21 +1,13 @@
 export const peerOptions = {
   config: {
     iceServers: [
-      // STUN сервери від Google
       { urls: 'stun:stun.l.google.com:19302' },
       { urls: 'stun:stun1.l.google.com:19302' },
       { urls: 'stun:stun2.l.google.com:19302' },
-      // TURN сервери для пробивання NAT/фаєрволів
-      {
-        urls: 'turn:openrelay.metered.ca:80',
-        username: 'openrelay',
-        credential: 'openrelay'
-      },
-      {
-        urls: 'turn:openrelay.metered.ca:443',
-        username: 'openrelay',
-        credential: 'openrelay'
-      }
+      { urls: 'stun:stun3.l.google.com:19302' },
+      { urls: 'stun:stun4.l.google.com:19302' },
+      { urls: 'stun:stun.services.mozilla.com' },
+      { urls: 'stun:stun.cloudflare.com:3478' }
     ]
   }
 };
